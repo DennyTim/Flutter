177e5533507f99bbf84dbc455311e1141dfab041
dety0819 <denys.tymokhin@netcracker.com>
2026-03-15 21:04:56 +0200

    hw4 - lesson4/homework.dart

--- bin/dart_learning_journey.dart
+++ bin/dart_learning_journey.dart
@@ -1,5 +1,5 @@
-import 'package:dart_learning_journey/dart_learning_journey.dart' as dart_learning_journey;
+import 'package:dart_learning_journey/lesson_04/homework.dart' as dart_learning_journey;
 
 void main(List<String> arguments) {
-  print('Hello world: ${dart_learning_journey.calculate()}!');
+  print('${dart_learning_journey.launchFns()}!');
 }
